3e4234ba1c071e4ee7ea45812fecb95930274a8b
RandomGuy52-egg <bojonase987@gmail.com>
2025-07-27 15:24:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let moolah = 0;
+
 function rollDice() {
   const dice = document.getElementById("dice");
 
@@ -5,4 +7,9 @@ function rollDice() {
   const randY = Math.floor(Math.random() * 4 + 1) * 90;
 
   dice.style.transform = `rotateX(${randX}deg) rotateY(${randY}deg)`;
+
+  // Add random amount of moolah (1–6)
+  const earnings = Math.floor(Math.random() * 6) + 1;
+  moolah += earnings;
+  document.getElementById("moolah").textContent = moolah;
 }
